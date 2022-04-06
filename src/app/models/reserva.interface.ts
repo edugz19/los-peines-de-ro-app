43a80e90d81a15ef2c7bf7356e60ed6490b0e1db
Edu García <edugz19@outlook.es>
2022-04-06 22:34:41 +0200

    [ADD] Reserva interface

---
+++ src/app/models/reserva.interface.ts
@@ -0,0 +1,10 @@
+export interface Reserva {
+    id: string;
+    uid: string;
+    servicio: string;
+    horaInicio: string;
+    horaFin: string;
+    fecha: string;
+    precio: number;
+    pagado: boolean;
+}
