d6ef26e22fc181fd2d3ea8de3204d3e932be6415
Eduardo García Zambrana <edugz19@outlook.es>
2023-12-13 21:01:47 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,23 +4,11 @@
 
 export const environment = {
   firebase: {
-    projectId: 'los-peines-de-ro',
-    appId: '1:1065261625373:web:8900fa27820245624c16f4',
-    storageBucket: 'los-peines-de-ro.appspot.com',
-    locationId: 'us-central',
-    apiKey: 'AIzaSyApbA33CtwYvl41sblhVLSktrGpLw3cH-Q',
-    authDomain: 'los-peines-de-ro.firebaseapp.com',
-    messagingSenderId: '1065261625373',
-    measurementId: 'G-EP3E7PXHQ2',
+    
   },
   production: false,
-  stripeKey: 'pk_test_51KX10dDbG1rPW2s6rLkUngYXPVGJZO51LXATQ50HacLoaNhjGcAxYdh4GTtrH87VlkiP4gLNIv9WUQ7QsUSL1pJj00M6PZVYkk',
-  paypal: {
-    clientId: 'AUzgp-Oy3AhP715O5cskSHHbXMV1lBmUqyzwM5FMhYMguKU48BytgQdKfkVh9m2mgc3OUnzlfOkoHOD6',
-    secret: 'ECawuCbTfHwpZwayj1jfr8Bm3gqKZ09LYpW8UapAwdXVb2FInxdekUP-f2NAC_356UR953QFYobhc0jQ'
-  },
+  stripeKey: '',
   // eslint-disable-next-line max-len
-  serverKey: 'AAAA-AaKcB0:APA91bEGbpGUAHVoWkIPHfpkvUACiS6Ct-cjdPlxaCWmvMdfeluF_P1FacssvRLbjZmVcnyRc-d6MsmMkZJxLBxltZUXTyKedQ56FIUcpF8HRSd4v0uZLLmIzooM7mqj2xD2SNWkeqQn'
 };
 
 /*
